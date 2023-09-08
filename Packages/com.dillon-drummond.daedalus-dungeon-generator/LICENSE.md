befa627d676ece3acd8c7999221b96a9c7f37587
Dillon Drummond <dillondrum70@gmail.com>
2023-09-08 12:50:48 -0400

    Adding licesne and headers to each script

--- Packages/com.dillon-drummond.daedalus-dungeon-generator/LICENSE.md
+++ Packages/com.dillon-drummond.daedalus-dungeon-generator/LICENSE.md
@@ -1,4 +1,4 @@
-Copyright (c) 2023 Dillon Drummond
+Copyright (c) 2022 - 2023 Dillon Drummond
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
--- Packages/com.dillon-drummond.daedalus-dungeon-generator/Runtime/Scripts/PriorityQueue.cs
+++ Packages/com.dillon-drummond.daedalus-dungeon-generator/Runtime/Scripts/PriorityQueue.cs
@@ -1,3 +1,39 @@
+/*
+        Copyright (c) 2022 - 2023 Dillon Drummond
+
+        Permission is hereby granted, free of charge, to any person obtaining
+        a copy of this software and associated documentation files (the
+        "Software"), to deal in the Software without restriction, including
+        without limitation the rights to use, copy, modify, merge, publish,
+        distribute, sublicense, and/or sell copies of the Software, and to
+        permit persons to whom the Software is furnished to do so, subject to
+        the following conditions:
+
+        The above copyright notice and this permission notice shall be
+        included in all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+        NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+        LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+        OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+        WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+        Daedalus Dungeon Generator: 3D Dungeon Generator Tool
+	    By Dillon W. Drummond
+        Adapted from https://www.dotnetlovers.com/article/231/priority-queue#:~:text=Implementation%20of%20Priority%20Queue%20using%20Heap&text=Unlike%20ordinary%20queues%2C%20a%20priority,highest%20priority%20can%20be%20fetched
+
+	    PriorityQueue.cs
+
+	    ********************************************
+	    ***  Implementation of a priority queue  ***
+	    ********************************************
+ */
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
